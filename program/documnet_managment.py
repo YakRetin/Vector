d2776b405cc11fb60e4211dc63e5ecf0cfdce5cf
YakRetin <123731989+YakRetin@users.noreply.github.com>
2024-04-19 12:28:50 +0300

    Create documnet_managment.py

---
+++ program/documnet_managment.py
@@ -0,0 +1,59 @@
+class ImportExport:
+    def init(self):
+        pass
+
+    def import_file(self):
+        pass
+
+    def export_file(self):
+        pass
+
+
+class MultiPageDocument:
+    def init(self):
+        pass
+
+    def create_page(self):
+        pass
+
+    def delete_page(self):
+        pass
+
+    def rearrange_pages(self):
+        pass
+
+
+class TechnicalTools:
+    def init(self):
+        pass
+
+    def add_ruler(self):
+        pass
+
+    def apply_grid(self):
+        pass
+
+    def align_objects(self):
+        pass
+
+
+class HistoryManagement:
+    def init(self):
+        pass
+
+    def undo_action(self):
+        pass
+
+    def redo_action(self):
+        pass
+
+
+class Printing:
+    def init(self):
+        pass
+
+    def set_print_settings(self):
+        pass
+
+    def preview_print(self):
+        pass
